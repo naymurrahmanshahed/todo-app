4031901371b5ad3d1d170c165d65ab8cd115133f
naymurrahmanshahed <shahed285@outlook.com>
2023-01-19 02:03:24 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,9 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer bg-gray-900 text-teal-600 font-light text-sm container mx-auto p-10 lg:max-w-4xl border-t border-dashed border-teal-900 rounded-bl-xl rounded-br-xl text-center ">
+      <p>&copy; {new Date().getFullYear()} Todo App. All rights reserved.</p>
+    </footer>
+  );
 };
 
 export default Footer;
